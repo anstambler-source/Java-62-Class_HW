2a1baa02d9e3d1a532cc18f9787975d0ca8cd26c
Anton Stambler <anstambler@gmail.com>
2025-11-17 09:54:14 +0200

    Delete New folder directory

--- New folder/src/components/FootButt.jsx
+++
@@ -1,7 +0,0 @@
-const FootButt = ({nameButt}) => {
-    return (
-        <div className="btn btn-danger border border-warning col-sm-2 offset-sm-3">{nameButt}</div>
-    )
-}
-
-export default FootButt;
